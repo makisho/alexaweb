e2a721de4a67cdd1da2fb2f9ed7bfee5dbb00e9b
makisho <makisho@gmail.com>
2016-12-30 10:38:50 +0100

    1

--- creds.py
+++ creds.py
@@ -1,9 +1,9 @@
 #Alexa
-Security_Profile_Description =""
-Security_Profile_ID  = ""
-Client_ID = ""
-Client_Secret = ""
-Product_ID = ""
+Security_Profile_Description ="MakishoAlexaWebSecurity"
+Security_Profile_ID  = "amzn1.application.2bb4ad3d65bb40499747a8ecc308d0c7"
+Client_ID = "amzn1.application-oa2-client.b7490a9eafe54d54b358991bd70b394a"
+Client_Secret = "bc577f8345920c6f89c816fbbaf607904b61d81989205ba3a142c2e75030497b"
+Product_ID = "MakishoAlexaWeb"
 
 #Redis
-redis_url = ""
+redis_url = "https://serene-citadel-18208.herokuapp.com/"
