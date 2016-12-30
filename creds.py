4a651af37214ad4c4a850a889dac8e55abc326e1
makisho <makisho@gmail.com>
2016-12-30 11:35:29 +0100

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,8 +1,8 @@
 #Alexa
 Security_Profile_Description ="MakishoAlexaWebSecurity"
 Security_Profile_ID  = "amzn1.application.2bb4ad3d65bb40499747a8ecc308d0c7"
-Client_ID = "amzn1.application-oa2-client.b7490a9eafe54d54b358991bd70b394a"
-Client_Secret = "bc577f8345920c6f89c816fbbaf607904b61d81989205ba3a142c2e75030497b"
+Client_ID = ""
+Client_Secret = ""
 Product_ID = "MakishoAlexaWeb"
 
 #Redis
